85c5ac4362dd61d1cc27238bcf4b06edfade51f2
jamvw <wuevufam@gmail.com>
2026-01-23 09:46:04 +0900

    [프로그래머스] L1 - 짝수와 홀수 (#224)

---
+++ programmers/L1_입문/224_짝수와_홀수.py
@@ -0,0 +1,12 @@
+# 짝수와 홀수
+# 프로그래머스 L1 (입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12937
+# 알고리즘: 기본연산, 조건문
+# 작성자: 표주하
+# 작성일: 2026. 01. 23. 09:46:04
+
+def solution(num):
+    if num % 2 == 0:
+        return 'Even'
+    else: 
+        return 'Odd'
